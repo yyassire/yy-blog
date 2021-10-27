86bd3b0ed1d4848d79fb5acca341ff7d4e790355
yyassire02 <yyassire02@gmail.com>
2021-10-27 13:21:56 +0300

    my final commit it everything goes well

--- server.js
+++ server.js
@@ -20,16 +20,26 @@ const db = process.env.DB;
 mongoose.connect(db).then(()=>{console.log("the server is connected to the db")}).catch(err=>console.log(err))
 
 // midleware
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"/client/build")))
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname, "client","build","index.html"))
-    })
-} else{
-    app.get("/",(req,res)=>{
-        res.send("Api running")
-    })
-}
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static(path.join(__dirname,"/client/build"))); 
+//     app.get('*', (req,res)=>{
+//         res.sendFile(path.join(__dirname, "client","build","index.html"))
+//     })
+    
+// } else{
+//     app.get("/",(req,res)=>{
+//         res.send("Api running")
+//     })
+// }
+
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+//   res.sendFile('client/build/index.html' , { root : __dirname})
+//   console.log(path.join(__dirname, "/client/build","index.html"))
+});
+
 
 
 app.listen(port,()=>{console.log("server up and running")})
